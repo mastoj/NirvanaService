dccfc8ca3e9d5a0b184a2c9b573da5592ceab08e
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-05 20:49:43 +0200

    chocolatey fix

--- chocolatey/nirvanaservice/tools/chocolateyInstall.ps1
+++ chocolatey/nirvanaservice/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
 $packageName = 'nirvanaservice' # arbitrary name for the package, used in messages
-$url = 'https://github.com/mastoj/NirvanaService/releases/NirvanaService-1.0.0.zip' # download url
+$url = 'https://github.com/mastoj/NirvanaService/blob/master/releases/NirvanaService-1.0.0.zip' # download url
 $silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
