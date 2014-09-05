53f6c99655d32d9b5c9067c90041167e9219e6ec
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-05 21:44:38 +0200

    done with v 1.0 and chocolate package work

--- chocolatey/nirvanaservice/tools/chocolateyInstall.ps1
+++ chocolatey/nirvanaservice/tools/chocolateyInstall.ps1
@@ -1,9 +1,35 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-
-$packageName = 'nirvanaservice' # arbitrary name for the package, used in messages
-$url = 'https://github.com/mastoj/NirvanaService/blob/master/releases/NirvanaService-1.0.0.zip' # download url
-$silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-# if removing $url64, please remove from here
+﻿$packageName = 'nirvanaservice' # arbitrary name for the package, used in messages
+$url = 'https://github.com/mastoj/NirvanaService/raw/master/releases/NirvanaService-1.0.0.zip' # download url
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
+  # downloader that the main helpers use to download items
+
+  # if removing $url64, please remove from here
+  #Get-ChocolateyWebFile "$packageName" 'DOWNLOAD_TO_FILE_FULL_PATH' "$url" "$url64"
+  # installer, will assert administrative rights - used by Install-ChocolateyPackage
+  #Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" '_FULLFILEPATH_' -validExitCodes $validExitCodes
+  # unzips a file to the specified location - auto overwrites existing content
+  #Get-ChocolateyUnzip "FULL_LOCATION_TO_ZIP.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  # Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
+  #Start-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell' -validExitCodes $validExitCodes
+  # add specific folders to the path - any executables found in the chocolatey package folder will already be on the path. This is used in addition to that or for cases when a native installer doesn't add things to the path.
+  #Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'User_OR_Machine' # Machine will assert administrative rights
+  # add specific files as shortcuts to the desktop
+  #$target = Join-Path $MyInvocation.MyCommand.Definition "$($packageName).exe"
+  #Install-ChocolateyDesktopLink $target
+
+  #------- ADDITIONAL SETUP -------#
+  # make sure to uncomment the error handling if you have additional setup to do
+
+  # outputs the bitness of the OS (either "32" or "64")
+  #$osBitness = Get-ProcessorBits
+
+
+  # the following is all part of error handling
+  #Write-ChocolateySuccess "$packageName"
+#} catch {
+  #Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  #throw
+#}
