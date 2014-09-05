dbf857ff097708c597acbd9d1cfeea878bb50bad
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-05 20:46:32 +0200

    choco pack and release v.1.0.0

---
+++ chocolatey/nirvanaservice/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+
+$packageName = 'nirvanaservice' # arbitrary name for the package, used in messages
+$url = 'https://github.com/mastoj/NirvanaService/releases/NirvanaService-1.0.0.zip' # download url
+$silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+
+# if removing $url64, please remove from here
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
