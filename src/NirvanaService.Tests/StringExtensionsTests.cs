a80d2ff1278d2ff0697c330dc878eb72a960d4b3
Tomas Jansson <tomas.jansson@gmail.com>
2014-09-11 20:25:34 +0200

    support for environment variables

---
+++ src/NirvanaService.Tests/StringExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NirvanaService.Tests
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Environment.SetEnvironmentVariable("TEST", "Hello");
+            Environment.SetEnvironmentVariable("TEST2", "World");
+        }
+
+        [Test]
+        public void ResolveEnvVariables_ShouldReturnSameStringIfNoEnvironmentVariables()
+        {
+            var str = "TESTING";
+            Assert.AreEqual("TESTING", str.ResolveEnvVariables());
+        }
+
+        [Test]
+        public void ResolveEnvVariables_ShouldReplaceOneVariable()
+        {
+            var str = "%TEST%ING";
+            Assert.AreEqual("HelloING", str.ResolveEnvVariables());
+
+        }
+
+        [Test]
+        public void ResolveEnvVariables_ShouldNotReplaceMissingVariable()
+        {
+            var str = "%MISSING%ING";
+            Assert.AreEqual("%MISSING%ING", str.ResolveEnvVariables());
+
+        }
+
+        [Test]
+        public void ResolveEnvVariables_ShouldReplaceAllVariables()
+        {
+            var str = "This is %TEST% some magic %TEST2% text";
+            Assert.AreEqual("This is Hello some magic World text", str.ResolveEnvVariables());
+
+        }
+    }
+}
---
+++ src/NirvanaService/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace NirvanaService
+{
+    public static class StringExtensions
+    {
+        public static string ResolveEnvVariables(this string text)
+        {
+            var regexp = new Regex(".*?(%.+?%).*?");
+            var matches = regexp.Matches(text);
+            foreach (Match match in matches)
+            {
+                var unescapedKey = match.Groups[1].Value;
+                var key = unescapedKey.Substring(1).Substring(0, unescapedKey.Length - 2);
+                var environmentVariable = Environment.GetEnvironmentVariable(key);
+                if (!string.IsNullOrWhiteSpace(environmentVariable))
+                {
+                    text = text.Replace(unescapedKey, environmentVariable);
+                }
+            }
+            return text;
+        }
+    }
+}
